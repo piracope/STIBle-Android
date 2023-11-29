47c5ea20df333f870a95644fbbfa4bf2480b13b8
58089 <58089@etu.he2b.be>
2023-11-29 13:13:30 +0100

    Start of setting up space for guesses

--- app/src/main/java/g58089/mobg5/stible/ui/GameScreen.kt
+++ app/src/main/java/g58089/mobg5/stible/ui/GameScreen.kt
@@ -1,12 +1,15 @@
 package g58089.mobg5.stible.ui
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,6 +23,7 @@ import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
 import g58089.mobg5.stible.model.GameRules
 import g58089.mobg5.stible.model.Route
+
 
 /**
  * Converts an "RRGGBB" string to the color's int representation.
@@ -38,7 +42,7 @@ private fun RouteSquare(route: Route) {
             .padding(8.dp) // TODO: set dimens in resources
             .clip(shape = RoundedCornerShape(3.dp))
             .background(Color(getColorFromRRGGBB(route.routeColor)))
-            .size(32.dp),
+            .size(32.dp)
     ) {
         Text(
             text = route.routeNumber.toString(),
@@ -51,16 +55,52 @@ private fun RouteSquare(route: Route) {
     }
 }
 
-
 @Composable
 fun GameScreen(gameRules: GameRules, modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
+        // Displaying the routes
         Row(Modifier.padding(8.dp)) {
             repeat(gameRules.puzzleRoutes.size) {
                 val currentRoute = gameRules.puzzleRoutes[it]
                 RouteSquare(currentRoute)
             }
+        }
 
+        Log.d("GameScreen", "guessCount = ${gameRules.maxGuessCount}")
+        // Displaying the guesses
+        Column {
+            repeat(gameRules.maxGuessCount) {
+                Row(
+                    modifier = Modifier.padding(2.dp),
+                    horizontalArrangement = Arrangement.spacedBy(2.dp)
+                ) {
+                    // TODO: figure out how to correctly set the width
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(MaterialTheme.colorScheme.outline)
+                            .size(16.dp)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(MaterialTheme.colorScheme.outline)
+                            .size(16.dp)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(MaterialTheme.colorScheme.outline)
+                            .size(16.dp)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(MaterialTheme.colorScheme.outline)
+                            .size(16.dp)
+                    )
+                }
+            }
         }
     }
 
