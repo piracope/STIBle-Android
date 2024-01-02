b0160b6e18b1db10346630c27d145ebbe7605897
58089 <58089@etu.he2b.be>
2024-01-03 00:52:13 +0100

    Add a little comment in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.1.2")
+
+    // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.6")
+
     // Icons
     implementation("androidx.compose.material:material-icons-extended")
     // TODO: this thing is big, use Proguard or whatever to minify it
